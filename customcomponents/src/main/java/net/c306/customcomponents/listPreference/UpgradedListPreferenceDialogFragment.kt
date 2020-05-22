9afa30cad23bea83d0454032b351fb42bbd18419
Aditya <ab.bulk@gmail.com>
2020-05-22 21:05:25 +0100

    v. 0.1.6.2
        [DONE] Add improved ListPreference
            [X] Add list preference to add list preference
            [X] Add list preference dialog and resources to add list preference
    
        [DONE] Add sample app
            [X] Test list preference with sample app
            [X] Test searchable multi select list preference with sample app
    
    Signed-off-by: Aditya <ab.bulk@gmail.com>

---
+++ customcomponents/src/main/java/net/c306/customcomponents/listPreference/UpgradedListPreferenceDialogFragment.kt
@@ -0,0 +1,181 @@
+package net.c306.customcomponents.listPreference
+
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.preference.PreferenceDialogFragmentCompat
+import kotlinx.android.synthetic.main.item_upgraded_list_preference.view.*
+import net.c306.customcomponents.R
+
+class UpgradedListPreferenceDialogFragment : PreferenceDialogFragmentCompat() {
+    
+    companion object {
+        fun newInstance(key: String): UpgradedListPreferenceDialogFragment =
+            UpgradedListPreferenceDialogFragment().apply {
+                arguments = Bundle(1).apply {
+                    putString(ARG_KEY, key)
+                }
+            }
+        
+        private const val SAVE_STATE_INDEX = "CustomListPreferenceDialogFragment.index"
+        private const val SAVE_STATE_ENTRIES = "CustomListPreferenceDialogFragment.entries"
+    }
+    
+    /* synthetic access */
+    private var mClickedDialogEntryIndex: Int = 0
+    private var mEntries: Array<UpgradedListPreference.Entry>? = null
+    
+    private val listAdapter: EntryAdapter by lazy {
+        context?.let {
+            EntryAdapter(
+                it,
+                mEntries ?: emptyArray(),
+                mClickedDialogEntryIndex
+            )
+        } ?: throw Exception("No Context found!")
+    }
+    
+    
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        if (savedInstanceState == null) {
+            
+            check(preference.entries != null) { "CustomListPreference requires an entries array." }
+            
+            mClickedDialogEntryIndex = preference.findIndexOfValue(preference.value)
+            
+            mEntries = preference.entries
+        }
+        else {
+            
+            mClickedDialogEntryIndex = savedInstanceState.getInt(SAVE_STATE_INDEX, 0)
+            
+            val savedEntries = savedInstanceState.getParcelableArray(SAVE_STATE_ENTRIES)
+            if (savedEntries != null && savedEntries.all { it is UpgradedListPreference.Entry }) {
+                @Suppress("UNCHECKED_CAST")
+                mEntries =  savedEntries as Array<UpgradedListPreference.Entry>
+            }
+        }
+    }
+    
+    
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(SAVE_STATE_INDEX, mClickedDialogEntryIndex)
+        outState.putParcelableArray(SAVE_STATE_ENTRIES, mEntries)
+        super.onSaveInstanceState(outState)
+    }
+    
+    
+    @SuppressLint("InflateParams")
+    override fun onPrepareDialogBuilder(builder: AlertDialog.Builder?) {
+        super.onPrepareDialogBuilder(builder)
+        
+        val contentView = activity?.layoutInflater?.inflate(R.layout.dialog_upgraded_list_preference, null)?.apply {
+            
+            // Populate the list and add click listener
+            findViewById<ListView>(R.id.list)?.apply {
+                adapter = listAdapter
+                onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                    
+                    if (mEntries?.get(position)?.enabled == false) {
+                        return@OnItemClickListener
+                    }
+                    
+                    mClickedDialogEntryIndex = position
+                    
+                    // Clicking on an item simulates the positive button click, and dismisses
+                    // the dialog.
+                    this@UpgradedListPreferenceDialogFragment.onClick(dialog,
+                                                                      DialogInterface.BUTTON_POSITIVE)
+                    dialog?.dismiss()
+                }
+            }
+            
+            // If dialog message is set, show it below the list else hide it
+            findViewById<TextView>(R.id.message)?.let {
+                if (preference.message != null) {
+                    it.text = preference.message
+                    it.visibility = View.VISIBLE
+                }
+                else {
+                    it.visibility = View.GONE
+                }
+            }
+            
+        }
+        
+        builder?.apply {
+            setTitle(preference.title)
+            
+            setView(contentView)
+            
+            // The typical interaction for list-based dialogs is to have click-on-an-item dismiss the
+            // dialog instead of the user having to press 'Ok'.
+            setPositiveButton(null, null)
+        }
+    }
+    
+    override fun getPreference(): UpgradedListPreference {
+        return super.getPreference() as UpgradedListPreference
+    }
+    
+    override fun onDialogClosed(positiveResult: Boolean) {
+        if (positiveResult && mClickedDialogEntryIndex >= 0) {
+            mEntries?.get(mClickedDialogEntryIndex)?.let {
+                if (preference.callChangeListener(it)) {
+                    preference.value = it.value.toString()
+                }
+            }
+        }
+    }
+    
+    
+    internal class EntryAdapter(context: Context, list: Array<UpgradedListPreference.Entry>, private val selectedEntry: Int)
+        : ArrayAdapter<UpgradedListPreference.Entry>(context, R.layout.item_upgraded_list_preference, list) {
+        
+        private val mInflater: LayoutInflater = LayoutInflater.from(context)
+        
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            return (convertView ?: mInflater.inflate(R.layout.item_upgraded_list_preference, null)).apply {
+                
+                val item = getItem(position) ?: return@apply
+                
+                with(text) {
+                    // Set enabled/disabled
+                    isEnabled = item.enabled
+                    
+                    isActivated = position == selectedEntry
+                    
+                    background = context.getDrawable(
+                        if (isActivated)
+                            R.color.bg_list_item_activated
+                        else
+                            android.R.color.transparent)
+                    
+                    // Set priority text
+                    text = item.entry
+                }
+                
+                // Set divider colour if `dividerBelow` is true, and not last item
+                divider.setBackgroundColor(if (item.dividerBelow && position < count - 1) context.getColor(R.color.divider) else context.getColor(android.R.color.transparent))
+                
+            }
+        }
+        
+        
+        
+    }
+    
+}
