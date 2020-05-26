ed1c9f59352da7dbb8470985a564280972702527
Aditya <ab.bulk@gmail.com>
2020-05-26 06:59:05 +0100

    v. 0.1.9.1
        [DONE] Merge CustomPreferenceUtils into CustomPreferenceFragment's companion object
    
    Signed-off-by: Aditya <ab.bulk@gmail.com>

--- customcomponents/src/main/java/net/c306/customcomponents/preference/CustomPreferenceFragment.kt
+++ customcomponents/src/main/java/net/c306/customcomponents/preference/CustomPreferenceFragment.kt
@@ -1,5 +1,7 @@
 package net.c306.customcomponents.preference
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
@@ -8,10 +10,59 @@ abstract class CustomPreferenceFragment: PreferenceFragmentCompat() {
     override fun onDisplayPreferenceDialog(preference: Preference?) {
         val fm = parentFragment?.childFragmentManager
         
-        if(CustomPreferenceUtils.displayPreferenceDialog(preference, fm, this))
+        if(displayPreferenceDialog(preference, fm, this))
             return
         
         super.onDisplayPreferenceDialog(preference)
+    }
+    
+    companion object {
+        /**
+         * Displays dialog for selected preference if it is a `CustomComponents` preference.
+         * @return true if it was a custom preference and been displayed
+         */
+        fun displayPreferenceDialog(
+            preference: Preference?,
+            fragmentManager: FragmentManager?,
+            targetFragment: Fragment
+        ): Boolean {
+            /**
+             * If this is our custom preference, inflate and show relevant dialog
+             */
+            when (preference) {
+                is TimePreference                      -> {
+                    TimePreferenceDialogFragment.newInstance(preference.key).run {
+                        fragmentManager?.let {
+                            setTargetFragment(targetFragment, 0)
+                            show(it, "android.support.v7.preference.PreferenceFragment.DIALOG")
+                        }
+                    }
+                }
+            
+                is UpgradedListPreference              -> {
+                    UpgradedListPreferenceDialogFragment.newInstance(preference.key).run {
+                        fragmentManager?.let {
+                            setTargetFragment(targetFragment, 0)
+                            show(it, "android.support.v7.preference.PreferenceFragment.DIALOG")
+                        }
+                    }
+                }
+            
+                is SearchableMultiSelectListPreference -> {
+                    SearchableMultiSelectListPreferenceDialogFragment.newInstance(preference.key).run {
+                        fragmentManager?.let {
+                            setTargetFragment(targetFragment, 0)
+                            show(it, "android.support.v7.preference.PreferenceFragment.DIALOG")
+                        }
+                    }
+                }
+            
+                else                                   -> return false
+            
+            }
+        
+            return true
+        }
     }
     
 }
